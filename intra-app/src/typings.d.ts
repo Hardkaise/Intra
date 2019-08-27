1cd16de6b4a55abd484c1c5da1b0f2ac8aa1f3bc
Clément Nancelle <clement.nancelle@epitech.eu>
2019-08-27 19:15:03 +0200

    [ADD] generated app with ts file

---
+++ intra-app/src/typings.d.ts
@@ -0,0 +1,16 @@
+
+// Typings. (Can be re-generated.)
+// !code: imports // !end
+// !code: init // !end
+
+declare module '*.json';
+
+// Application.
+
+// Seed.
+declare module '@feathers-plus/test-utils';
+
+// !code: more // !end
+
+// !code: funcs // !end
+// !code: end // !end
